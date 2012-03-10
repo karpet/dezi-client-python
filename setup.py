7991145aefa6ed8afa2ff82ded8d988c14b365fe
Peter Karman <karpet@peknet.com>
2012-03-09 22:41:31 -0600

    test and comment on build

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(
   name    = "dezi_client",
   version = "0.001",
-  py_modules = [ "Dezi.Client", "Dezi.Doc", "Dezi.Response" ],
+  packages = [ "Dezi" ],
 )
